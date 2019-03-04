4ac679ccbb3e49fad7443f19e1c8feeae1d53786
Минин А.В <MA@dc.transoil.gomel.by>
2019-03-04 21:09:47 +0300

    all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    for (let i = 0; i < str.length; i++){
+        for (let j = 0; j < bracketsConfig.length; j++){
+            if (bracketsConfig[j][1] === str[i] && bracketsConfig[j][0] === stack[stack.length-1]){
+                stack.pop();
+            } else
+            if (bracketsConfig[j][0] === str[i]){
+                stack.push(str[i]);
+            }
+
+        }
+    }
+    if (stack.length === 0) return true;
+    else return false;
 }
